e5f7dd48345ca84760bef5baf189515f3bcbc8cb
R-Alejandro <AlejandroRHiguera@gmail.com>
2021-03-31 02:35:24 -0500

    Class-Based Views

--- blog/miblog/urls.py
+++ blog/miblog/urls.py
@@ -4,7 +4,7 @@ from . import views
 #generar las url para las vistas
 app_name = 'miblog'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('categoria/<int:cat_id>', views.programacion, name='prog'),
-    path('post/<int:post_id>', views.post, name='post')
+    path('', views.Index.as_view(), name='index'),
+    path('categoria/<int:cat_id>', views.ListCategoria.as_view(), name='prog'),
+    path('post/<int:post_id>', views.ShowPost.as_view(), name='post')
 ]
