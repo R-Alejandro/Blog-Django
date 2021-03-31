b409864cfe4b658f3d2b341218ce70ea0a3af4a2
R-Alejandro <AlejandroRHiguera@gmail.com>
2021-03-30 21:02:23 -0500

    paginacion

--- blog/miblog/views.py
+++ blog/miblog/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import *
 # Create your views here.
 #views based on functions (for now)
 def index(request):
-    return render(request, 'miblog/index.html', )
+    return render(request, 'miblog/index.html')
 
 def programacion(request, cat_id):
-    posts = Post.objects.filter(categoria_id_id=cat_id)
+    posts = Post.objects.filter(categoria_id_id=cat_id) 
+    paginator = Paginator(posts, 1) #numero de objetos por pagina
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     tit = Categoria.objects.get(id=cat_id)
     tit = tit.nombre
-    return render(request, 'miblog/prog.html', {'posts': posts, 'titulo': tit})
+    content = {'posts': posts, 'titulo': tit, 'pg_obj':page_obj}
+    return render(request, 'miblog/prog.html', content)
 
 
 def post(request, post_id):
